6bfef17c1570e8a4a308aa209b735204d867e836
CHARLES Samuel <charles.samuel.cha@gmail.com>
2026-06-04 11:50:46 +0200

    Modification exception

--- serveurWeb/bin/LectureXML.java
+++ serveurWeb/bin/LectureXML.java
@@ -5,42 +5,36 @@ import javax.xml.parsers.*;
  * Classe permettant de lire un fichier .xml
  */
 public class LectureXML {
-    public static String[] chargerConfig(String fichier) {
+    public static String[] chargerConfig(String fichier) throws Exception{
 
         Document document = null;
         DocumentBuilderFactory factory = null;
-        String[] tabReponse = null;
+        
+        factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        document = builder.parse(fichier);
 
-        try {
-            // recupere le fichier xml
-            factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            document = builder.parse(fichier);
+        Element racine = document.getDocumentElement();
 
-            Element racine = document.getDocumentElement();
+        NodeList racineNoeuds = racine.getChildNodes();
+        
+        String[] tabReponse = new String[5];
 
-            NodeList racineNoeuds = racine.getChildNodes();
-            
-            tabReponse = new String[5];
+        Element port = (Element) racine.getElementsByTagName("port").item(0);
+        tabReponse[0] = port.getTextContent();
 
-            Element port = (Element) racine.getElementsByTagName("port").item(0);
-            tabReponse[0] = port.getTextContent();
+        Element docRoot = (Element) racine.getElementsByTagName("DocumentRoot").item(0);
+        tabReponse[1] = docRoot.getTextContent();
 
-            Element docRoot = (Element) racine.getElementsByTagName("DocumentRoot").item(0);
-            tabReponse[1] = docRoot.getTextContent();
+        Element defIndex = (Element) racine.getElementsByTagName("Default").item(0);
+        tabReponse[2] = defIndex.getTextContent();
 
-            Element defIndex = (Element) racine.getElementsByTagName("Default").item(0);
-            tabReponse[2] = defIndex.getTextContent();
+        Element logAcc = (Element) racine.getElementsByTagName("Acceslog").item(0);
+        tabReponse[3] = logAcc.getTextContent();
 
-            Element logAcc = (Element) racine.getElementsByTagName("Acceslog").item(0);
-            tabReponse[3] = logAcc.getTextContent();
+        Element errLog = (Element) racine.getElementsByTagName("Errorlog").item(0);
+        tabReponse[4] = errLog.getTextContent();
 
-            Element errLog = (Element) racine.getElementsByTagName("Errorlog").item(0);
-            tabReponse[4] = errLog.getTextContent();
-
-        } catch (Exception e) {
-        e.printStackTrace();
-        }
         return tabReponse;
     }
 }

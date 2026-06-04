3a2d6a55f6ab74d1a3a474cfea76b17a38f23806
CHARLES Samuel <charles.samuel.cha@gmail.com>
2026-06-04 11:44:20 +0200

    Ajout methode static chargerConfig

--- serveurWeb/bin/LectureXML.java
+++ serveurWeb/bin/LectureXML.java
@@ -1,6 +1,46 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
 /**
  * Classe permettant de lire un fichier .xml
  */
 public class LectureXML {
+    public static String[] chargerConfig(String fichier) {
 
+        Document document = null;
+        DocumentBuilderFactory factory = null;
+        String[] tabReponse = null;
+
+        try {
+            // recupere le fichier xml
+            factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse(fichier);
+
+            Element racine = document.getDocumentElement();
+
+            NodeList racineNoeuds = racine.getChildNodes();
+            
+            tabReponse = new String[5];
+
+            Element port = (Element) racine.getElementsByTagName("port").item(0);
+            tabReponse[0] = port.getTextContent();
+
+            Element docRoot = (Element) racine.getElementsByTagName("DocumentRoot").item(0);
+            tabReponse[1] = docRoot.getTextContent();
+
+            Element defIndex = (Element) racine.getElementsByTagName("Default").item(0);
+            tabReponse[2] = defIndex.getTextContent();
+
+            Element logAcc = (Element) racine.getElementsByTagName("Acceslog").item(0);
+            tabReponse[3] = logAcc.getTextContent();
+
+            Element errLog = (Element) racine.getElementsByTagName("Errorlog").item(0);
+            tabReponse[4] = errLog.getTextContent();
+
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+        return tabReponse;
+    }
 }

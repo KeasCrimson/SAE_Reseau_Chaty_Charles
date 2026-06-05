4b748826ab6785544cc06b42b639b11dd36f0bf0
Keas <aloys.chaty9@etu.univ-lorraine.fr>
2026-06-05 06:43:25 +0000

    modification du main

--- serveurWeb/bin/ServeurMain.java
+++ serveurWeb/bin/ServeurMain.java
@@ -13,54 +13,61 @@ import java.util.Scanner;
  */
 public class ServeurMain {
     public static void main(String[] args) throws IOException {
+
+        String[][] config = LectureXML.chargerConfig("../conf.d/serverWeb.conf");
         // recupere le port
-        int port = Integer.parseInt(args[0]);
-        // ouvre le socket serveur avec ce port
-        ServerSocket serv = new ServerSocket(port);
+        for(int nConfig = 0; nConfig<config.length; nConfig++){
+            int port = Integer.parseInt(config[nConfig][0]);
+            // ouvre le socket serveur avec ce port
+            Thread site = new Thread(() -> {
+                ServerSocket serv = new ServerSocket(port);
+            // tant que vrai
+                while (true){
+                    try{
+                        Socket client = serv.accept();
+                        // on initialise la sortie
+                        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+                        // on initialise l'entree
+                        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-        // tant que vrai
-        while (true){
-            // le socket client accept le serveur
-            Socket client = serv.accept();
-            // on initialise la sortie
-            PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-            // on initialise l'entree
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        // lecture de la requete
+                        String ligne = in.readLine();
+                        System.out.println(ligne); //
 
-            // lecture de la requete
-            String ligne = in.readLine();
-            System.out.println(ligne); //
+                        // si la requete n'est pas nul et qu'elle contient "GET"
+                        if(ligne != null && ligne.contains("GET")){
+                            // split la ligne pour ne recuperer que le chemin demande
+                            String[] chemin = ligne.split(" ");
+                            // je rajoute un point devant pour construire un vrai chemin d'acces
+                            String file = config[nConfig][1] + chemin[1];
+                            System.out.println(file);
 
-            // si la requete n'est pas nul et qu'elle contient "GET"
-            if(ligne != null && ligne.contains("GET")){
-                // split la ligne pour ne recuperer que le chemin demande
-                String[] chemin = ligne.split(" ");
-                // je rajoute un point devant pour construire un vrai chemin d'acces
-                String file = "." + chemin[1];
-                System.out.println(file);
-
-                try{
-                    // essaye de lire le fichier et de le mettre dans un tableau d'octet
-                    byte[] tab = Files.readAllBytes(Paths.get(file));
-                    // creation de la reponse
-                    String mess =   "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: text/html; charset=iso-8859-1\r\n" +
-                            "Connection : Keep-Alive\r\n" +
-                            "File Data: 30 bytes\r\n";
-                    
-                    out.println(mess);
-                    client.getOutputStream().write(tab);
-                    // force l'envoi immediat des donnees
-                    client.getOutputStream().flush();
-                } catch (java.nio.file.NoSuchFileException e){
-                    // si le fichier n'est pas trouve on lance l'erreur 404
-                    System.out.println("Fichier non trouvé : " + file);
-                    String erreur404 = "HTTP/1.1 404 Not Found\r\n\r\n<h1>Erreur 404 : Fichier introuvable</h1>";
-                    out.print(erreur404);
-                    out.flush();
+                            try{
+                                // essaye de lire le fichier et de le mettre dans un tableau d'octet
+                                byte[] tab = Files.readAllBytes(Paths.get(file));
+                                // creation de la reponse
+                                String mess =   "HTTP/1.1 200 OK\r\n" +
+                                        "Content-Type: text/html; charset=iso-8859-1\r\n" +
+                                        "Connection : Keep-Alive\r\n" +
+                                        "File Data: 30 bytes\r\n";
+                                
+                                out.println(mess);
+                                client.getOutputStream().write(tab);
+                                // force l'envoi immediat des donnees
+                                client.getOutputStream().flush();
+                            } catch (java.nio.file.NoSuchFileException e){
+                                // si le fichier n'est pas trouve on lance l'erreur 404
+                                System.out.println("Fichier non trouvé : " + file);
+                                String erreur404 = "HTTP/1.1 404 Not Found\r\n\r\n<h1>Erreur 404 : Fichier introuvable</h1>";
+                                System.out.println(erreur404);
+                            }
+                        }
+                    } catch (IOException e){
+                        System.out.println("Problème de lecture du fichier");
+                    }
                 }
-            }
-            client.close();
+            });
+            site.start();
         }
     }
 }
